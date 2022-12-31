905d9d8b710dd046b35cf26218455656e36deb41
Risab <risabvishwakarma@hotmail.com>
2022-12-31 19:38:46 +0530

    final comit

---
+++ Git_ke_liye/src/main/java/com/unitral/git_ke_liye/GitKeLiyeApplication.java
@@ -0,0 +1,13 @@
+package com.unitral.git_ke_liye;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitKeLiyeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitKeLiyeApplication.class, args);
+	}
+
+}
---
+++ Git_ke_liye/src/test/java/com/unitral/git_ke_liye/GitKeLiyeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.unitral.git_ke_liye;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitKeLiyeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
